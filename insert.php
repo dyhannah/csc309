b135b333ad585be186af955bff56de5c5ca854e5
dyhannah <139907709+dyhannah@users.noreply.github.com>
2023-07-18 13:18:42 -0400

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,8 +1,7 @@
-
 <?php
     $server = 'localhost'; // 127.0.0.1
     $username = 'root';
-    $password = 'rootroot';
+    $password = '';
     $db = 'csc309';
 
     // Open a new connection
@@ -14,8 +13,8 @@
     }
 
     // Create a Table
-    $sql = "INSERT INTO users (firstname, lastname, email) 
-    VALUES ('Nwafor', 'Benjamin', 'benjaminn@gmail.com')";
+    $sql = "INSERT INTO users (firstname, lastname, email, gender, date_of_birth) 
+    VALUES ('DIANA', 'JOHNSON', dyhannahjhunsen@gmail.com', 'F', '17-05-2001')";
 
     $result = $con->query($sql);
 
